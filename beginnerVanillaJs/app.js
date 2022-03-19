953ca74902ac61f48b38c05c3b2d7a706b8d9c7e
Chawintee <Chawinteetea@gmail.com>
2022-03-19 08:44:04 +0700

    add todo to todolist

---
+++ beginnerVanillaJs/app.js
@@ -0,0 +1,42 @@
+// Selectors
+const todoInput = document.querySelector('.todo-input');
+const todoButton = document.querySelector('.todo-button');
+const todoList = document.querySelector('.todo-list');
+
+// Event Listeners
+todoButton.addEventListener('click',addTodo);
+
+
+
+
+// Function
+
+function addTodo(event){
+    //Prevent form from submitting
+    event.preventDefault();
+    //Todo DIV
+    const todoDiv = document.createElement('div');
+    todoDiv.classList.add('todo');
+    //Create LI
+    const newTodo = document.createElement('li');
+    // newTodo.innerText = 'hey';
+    newTodo.innerText = todoInput.value;
+    newTodo.classList.add('todo-item');
+    todoDiv.appendChild(newTodo);
+    //CHECK MARK BUTTON
+    const completeButton = document.createElement('button');
+    completeButton.innerHTML = '<i class="fas fa-check"></i>';
+    completeButton.classList.add('complete-btn');
+    todoDiv.appendChild(completeButton);
+    //CHECK trash BUTTON
+    const trashButton = document.createElement('button');
+    trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+    trashButton.classList.add('trash-btn');
+    todoDiv.appendChild(trashButton);
+    //APPEND TODO LIST
+    todoList.appendChild(todoDiv)
+    //Clear Todo INPUT VALUE
+    todoInput.value = "";
+
+    console.log(todoInput);
+}
